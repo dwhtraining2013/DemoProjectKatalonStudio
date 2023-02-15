627a2b2b0498443e28cb1cd898897bfc4a5ad555
dell <dell@DESKTOP-F0MOP2V>
2023-02-15 11:01:25 +0530

    Updated DemoCheckpoints test case to use Checkpoint-2 instead of Checkpoint-1

--- Scripts/DemoCheckpoints/Script1676086191179.groovy
+++ Scripts/DemoCheckpoints/Script1676086191179.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.verifyCheckpoint(findCheckpoint('Checkpoints/Checkpoint-1'), true)
+WebUI.verifyCheckpoint(findCheckpoint('Checkpoints/Checkpoint-2'), true)
 
