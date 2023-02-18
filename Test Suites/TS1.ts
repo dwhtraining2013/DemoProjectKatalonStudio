819ce2cb0ef9fdd9e2f65d3f8ad3e4da4f2cfe28
dell <dell@DESKTOP-F0MOP2V>
2023-02-18 10:24:23 +0530

    First commit on 18th Feb 2023

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>acb5f3a3-6e80-4bb1-a5fd-f6a0ba785a45</testSuiteGuid>
+   <testCaseLink>
+      <guid>cfaa665d-9f2a-4f24-8df5-0addee1e2824</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
